cef652495d4f6120c2cfd0adb196138c5c10aa2b
Graham <graham.morgan@ncl.ac.uk>
2020-06-11 19:49:34 +0100

    Roman Style Added 2

--- docs/code0.js
+++ docs/code0.js
@@ -1,12 +1,8 @@
 gdjs.NewSceneCode = {};
-gdjs.NewSceneCode.GDcompassObjects1= [];
-gdjs.NewSceneCode.GDcompassObjects2= [];
-gdjs.NewSceneCode.GDneedleObjects1= [];
-gdjs.NewSceneCode.GDneedleObjects2= [];
 gdjs.NewSceneCode.GDRomanCompassObjects1= [];
 gdjs.NewSceneCode.GDRomanCompassObjects2= [];
-gdjs.NewSceneCode.GDRomadnNeedleObjects1= [];
-gdjs.NewSceneCode.GDRomadnNeedleObjects2= [];
+gdjs.NewSceneCode.GDOldNeedleObjects1= [];
+gdjs.NewSceneCode.GDOldNeedleObjects2= [];
 
 gdjs.NewSceneCode.conditionTrue_0 = {val:false};
 gdjs.NewSceneCode.condition0IsTrue_0 = {val:false};
@@ -39,9 +35,9 @@ gdjs.NewSceneCode.condition0IsTrue_0.val = gdjs.evtTools.runtimeScene.sceneJustB
 
 
 {
-gdjs.NewSceneCode.GDRomadnNeedleObjects1.createFrom(runtimeScene.getObjects("RomadnNeedle"));
-{for(var i = 0, len = gdjs.NewSceneCode.GDRomadnNeedleObjects1.length ;i < len;++i) {
-    gdjs.NewSceneCode.GDRomadnNeedleObjects1[i].setAngle(gdjs.deviceSensors.orientation.getOrientationAlpha());
+gdjs.NewSceneCode.GDOldNeedleObjects1.createFrom(runtimeScene.getObjects("OldNeedle"));
+{for(var i = 0, len = gdjs.NewSceneCode.GDOldNeedleObjects1.length ;i < len;++i) {
+    gdjs.NewSceneCode.GDOldNeedleObjects1[i].setAngle(gdjs.deviceSensors.orientation.getOrientationAlpha());
 }
 }}
 
@@ -54,14 +50,10 @@ gdjs.NewSceneCode.GDRomadnNeedleObjects1.createFrom(runtimeScene.getObjects("Rom
 gdjs.NewSceneCode.func = function(runtimeScene) {
 runtimeScene.getOnceTriggers().startNewFrame();
 
-gdjs.NewSceneCode.GDcompassObjects1.length = 0;
-gdjs.NewSceneCode.GDcompassObjects2.length = 0;
-gdjs.NewSceneCode.GDneedleObjects1.length = 0;
-gdjs.NewSceneCode.GDneedleObjects2.length = 0;
 gdjs.NewSceneCode.GDRomanCompassObjects1.length = 0;
 gdjs.NewSceneCode.GDRomanCompassObjects2.length = 0;
-gdjs.NewSceneCode.GDRomadnNeedleObjects1.length = 0;
-gdjs.NewSceneCode.GDRomadnNeedleObjects2.length = 0;
+gdjs.NewSceneCode.GDOldNeedleObjects1.length = 0;
+gdjs.NewSceneCode.GDOldNeedleObjects2.length = 0;
 
 gdjs.NewSceneCode.eventsList0x5b71c8(runtimeScene);
 return;
